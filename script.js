7bf2e1366acc1ec5e2e31a0a47c8338a4b4d140b
programmerAju <programmerhead7@gmail.com>
2023-12-11 08:03:47 +0530

    about us  and conctact us added

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 3000);
 }
+
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";    
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";    
+}
+
+
+const scriptURL = ''
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Sucessfully"
+        setTimeout(function(){
+            msg.innerHTML = "" 
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
